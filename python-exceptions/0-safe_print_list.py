74587d62496e4a16d579e06ad83725603580760d
root <9548holbertonstudents.com>
2024-09-09 15:41:15 +0200

    prints x elements of a list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
+     p = 0
     try:
         for i in range(x):
-            print(my_list[i], end=' ')
-            count += 1
-        print()
+            print(my_list[i], end="")
+            p += 1
     except IndexError:
-        print()
-        return count
-
-    return count
+        pass
+    print()
+    return p
