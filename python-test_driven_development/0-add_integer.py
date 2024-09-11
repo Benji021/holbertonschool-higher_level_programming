aadaafe7ac8e01555563cf6ba5994b2ae0b439c3
Drakan <benjaminpignolet@orange.fr>
2024-09-11 13:52:02 +0000

    adds 2 integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This is a docstring."""
+"""
+This is a docstring.
+"""
 def add_integer(a, b=98):
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
