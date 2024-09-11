f603ca80513962e6672755608cb2e77496073c48
Drakan <benjaminpignolet@orange.fr>
2024-09-11 13:24:47 +0000

    prints a text with 2 new lines after each of these characters: .  ? and :

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,18 +7,16 @@ def text_indentation(text):
         raise TypeError("text must be a string")
 
     result = ""
-    skip_space = True  # Flag to skip spaces at the beginning of a new sentence
+    skip_space = True
 
     for char in text:
         result += char
         if char in ".?:":
             result += "\n\n"
-            skip_space = True  # We want to skip spaces after these characters
+            skip_space = True
         elif char == " " and skip_space:
-            # Skip the space after punctuation
             continue
         else:
             skip_space = False
 
-    # Print the result without leading/trailing spaces on each line
-    print(result.strip())
+        print(result.strip())
