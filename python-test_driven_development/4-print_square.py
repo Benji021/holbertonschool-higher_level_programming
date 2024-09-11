99ef8138fe475d6fcba2088d1fb7719611a641e9
Drakan <benjaminpignolet@orange.fr>
2024-09-11 13:29:25 +0000

    prints a square with the character #

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This module contains a function that prints a square with the character '#'.
+"""
 def print_square(size):
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
