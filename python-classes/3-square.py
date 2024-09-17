8ac5fcab98287160f6764b6a2ab3fe70a9d18fd5
Benji021 <benjaminpignolet@orange.fr>
2024-09-17 09:46:08 +0200

    return the current square area

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,12 +2,12 @@
 """Define a square"""
 
 class Square:
-    """Empty class that defines a square"""
+    """Class that defines a square"""
 
     def __int__(self, size=0):
         """Initialize a new square with an optimal size"""
         if not isinstance(size, int):
-            raise TypeError("size must be an integers")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
