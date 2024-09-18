dc5fc6045d3941f8003c6f5685b8e83b7aeaa002
Benji021 <9548holbertonstudents.com>
2024-09-18 10:55:22 +0200

    Return the area of the square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,8 +10,7 @@ class Square:
     def size(self):
         """Retrieve the size of the square"""
         return self.__size
-
-
+    
     @size.setter   
     def size(self, value):
         """Set the size of the square with validation"""
@@ -20,7 +19,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
 
     def area(self):
         """Return the area of the square"""
