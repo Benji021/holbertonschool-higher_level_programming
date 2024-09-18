8132c4dc8445055ecb1c0949b8df9701f38a9a56
Benji021 <9548holbertonstudents.com>
2024-09-18 10:15:57 +0200

    Initialize the square with a square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """Define a square"""
 
+
 class Square:
     """Class that defines a square"""
+
 
 def __init__(self, size):
     """Initialize the square with a size"""
     self.__size = size
+    
