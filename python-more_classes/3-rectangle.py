5c34dc70257b71776ba1ac61bf4aa9c191aa7834
Benji021 <9548holbertonstudents.com>
2024-09-19 11:59:13 +0200

    print to the rectangle with str

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """
         Initialize a new rectangle
-        
+
         Args:
         width (int): width of the rectangle
         height (int): height of the rectangle
@@ -20,15 +20,15 @@ class Rectangle:
     def width(self):
         """Get the width of the rectangle"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """
         Set the width of the rectangle
-        
+
         Args:
         value (int): the width of the rectangle
-        
+
         Raises:
         TypeError: If width is not integer;
         ValueError: If width is less than 0;
@@ -37,7 +37,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        
+
     @property
     def height(self):
         """Get the height of a rectangle"""
@@ -47,10 +47,10 @@ class Rectangle:
     def height(self, value):
         """
         Set the height of the rectangle
-        
+
         Args:
         value (int): the height of the rectangle
-        
+
         Raises:
         TypeError: If height is not integer;
         ValueError: If height is less than 0;
@@ -64,19 +64,19 @@ class Rectangle:
     def area(self):
         """Return the rectangle area"""
         return self.__width * self.__height
-    
+
     def perimeter(self):
         """Return the rectangle perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return 2 * (self.__width + self.__height)
-        
+ 
     def __str__(self):
         """Checks if width and height are null"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        
+
         line = "#" * self.__width
 
         rect = [line]
