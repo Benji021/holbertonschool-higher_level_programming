9fa4e0ec50f34e02c041d2e21619de224cd7e5d8
Benji021 <9548holbertonstudents.com>
2024-09-20 10:41:07 +0200

    return the area of the square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,40 +1,25 @@
 #!/usr/bin/python3
-"""
-This module will define a square class.
-"""
+"""Define a square"""
 
 
 class Square:
-    """
-    This is a square class.
-    """
+    """Class that defines a square"""
+
     def __init__(self, size=0):
-        """
-         Creating instances of a square with one parameter.
-
-         Args:
-            size: will be the size of the square
-
-         Raises:
-            TypeError: if size is not an integer
-            ValueError: if size is less than zero
-         """
-
+        """Initialize a new square with an optimal size"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
         if size < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = size
 
-    @property
-    def size(self):
+@property
+def size(self):
         """Retrieve the size of the square"""
         return self.__size
     
-    @size.setter
-    def size(self, value):
+@size.setter
+def size(self, value):
         """
         Set the size of the square with validation
         
@@ -51,11 +36,6 @@ class Square:
             ValueError("size must be >= 0")
         self.__size = value
 
-    def area(self):
-        """
-        Determines the area of a square.
-
-        Returns:
-            int: The size of a square depending on the size
-        """
-        return self.__size * self.__size
+def area(self):
+       """return the current square area"""
+       return self.__size ** 2
