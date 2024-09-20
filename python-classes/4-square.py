87c58a3d75e9a3fefdd211b6727fd281e029a5dd
Benji021 <9548holbertonstudents.com>
2024-09-20 10:43:08 +0200

    return the area of the square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -13,13 +13,13 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-@property
-def size(self):
+    @property
+    def size(self):
         """Retrieve the size of the square"""
         return self.__size
     
-@size.setter
-def size(self, value):
+    @size.setter
+    def size(self, value):
         """
         Set the size of the square with validation
         
@@ -36,6 +36,6 @@ def size(self, value):
             ValueError("size must be >= 0")
         self.__size = value
 
-def area(self):
+    def area(self):
        """return the current square area"""
        return self.__size ** 2
