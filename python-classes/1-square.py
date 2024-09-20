d6be9fd76d355405906edbf4517027df44132230
Benji021 <9548holbertonstudents.com>
2024-09-20 10:17:04 +0200

    initialize the square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,11 +5,11 @@
 class Square:
     """defines a square"""
 
-def __init__(self, size=0):
+def __init__(self, size):
     """
     Initializes the square with a private size attribute
     
     Args:
-    size: will be the size of the square
+    size (int): size of the square
     """
     self.__size = size
