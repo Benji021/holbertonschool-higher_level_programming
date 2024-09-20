09ba4ef848c3de4fc85c0ef0ad569aefb3d7af1a
Benji021 <9548holbertonstudents.com>
2024-09-20 10:45:37 +0200

    return the area of the square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,7 +6,16 @@ class Square:
     """Class that defines a square"""
 
     def __init__(self, size=0):
-        """Initialize a new square with an optimal size"""
+        """
+        Initialize a new square with an optimal size
+        
+        Args:
+        size (int): size of the square
+        
+        Raises:
+        TypeError: If size is not interger;
+        ValueError: If size is less than 0;
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
