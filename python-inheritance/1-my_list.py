f3eba38151ca22d42d065117beb05e2698c9143c
Benji021 <9548holbertonstudents.com>
2024-09-23 11:05:33 +0200

    MyList

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defining a class MyList"""
+
+
+class MyList(list):
+    """Class MyList that inherits from list"""
+
+    def print_sorted(self):
+        """Print sorted list"""
+        print(soted(self))
