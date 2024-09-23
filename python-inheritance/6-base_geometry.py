0a7111ca50f7d9f3f9683662f55d5e8ae9bd2a79
Benji021 <9548holbertonstudents.com>
2024-09-23 15:35:08 +0200

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Docstring Improve Geometry"""
+
+
+class BaseGeometry:
+    """Empty class that defines BaseGeometry"""
+
+    def area(self):
+        """
+        Return the current area that raises an exeption
+        with message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
