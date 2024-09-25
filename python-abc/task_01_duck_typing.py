cd0f15a8a6b2fe08e89e2374613b57977d953260
Benji021 <9548holbertonstudents.com>
2024-09-25 09:46:14 +0200

    Shape, interface and duck typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -15,13 +15,13 @@ class Shape(ABC):
         pass
 
 
-class circle(Shape):
+class Circle(Shape):
 
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        return math.pi * self.radius
+        return math.pi * (self.radius ** 2)
 
     def perimeter(self):
         return 2 * math.pi * self.radius
