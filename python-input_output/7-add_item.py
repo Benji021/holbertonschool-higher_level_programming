dcf39f9cced8cea28822af717e66a43cfb4c24ec
Benji021 <9548holbertonstudents.com>
2024-09-30 13:44:49 +0200

    Load, add and save to file JSON

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,16 +2,19 @@
 """Docstring Load, add and save to JSON"""
 
 
-import json
+import sys
+import os
 
-def save_to_json_file(my_obj, filename):
-    """Saves an object to a text file using JSON representation"""
-    with open(filename, 'w') as f:
-        json.dump(my_obj, f)
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
-import json
+filename = "add_item.json"
 
-def load_from_json_file(filename):
-    """Loads an object from a text file containing JSON representation"""
-    with open(filename, 'r') as f:
-        return json.load(f)
+if os.path.exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+
+save_to_json_file(my_list, filename)
