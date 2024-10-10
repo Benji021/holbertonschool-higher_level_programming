c0348bbfe51a1854758874740f3b6fc230551e39
Benji021 <9548holbertonstudents.com>
2024-10-10 14:55:39 +0200

    request

--- restful-api/test_requests.py
+++ restful-api/test_requests.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
  
 import requests
+import csv
 
-# Afficher la version de la bibliothèque requests
-print(f"Requests version: {requests.__version__}")
+r = requests.get()
+
+# Recover fetches all post from JSONPlaceholder and print status code
+def fetch_and_print_posts():
+    url = https://jsonplaceholder.typicode.com/posts
+
+    response = requests.get(url)
+
+    for post in posts:
+        print(post["title"])
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        print("title of all the posts")
+
+    else:
+        print("failed to fetch posts")
+
+def fetch_and_save_posts():
