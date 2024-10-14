99b8e8972386d60597a4108bf3b15e06f8f5a683
Benji021 <9548holbertonstudents.com>
2024-10-14 10:02:47 +0200

    request

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,7 +6,7 @@ import csv
 
 # Recover fetches all post from JSONPlaceholder and print status code
 def fetch_and_print_posts():
-    url = https://jsonplaceholder.typicode.com/posts
+    url = "https://jsonplaceholder.typicode.com/posts"
 
     response = requests.get(url)
 
@@ -20,7 +20,7 @@ def fetch_and_print_posts():
         print("failed to fetch posts")
 
 def fetch_and_save_posts():
-    url = https://jsonplaceholder.typicode.com/posts
+    url = "https://jsonplaceholder.typicode.com/posts"
 
     response = requests.get(url)
 
@@ -29,14 +29,14 @@ def fetch_and_save_posts():
 
     structured_data = [{
         "id": post["id"], "title": post["title"],
-        "body": post["body"]}for post in posts]
+        "body": post["body"]} for post in posts]
     
-    filedname = ["id", "title", "body"]
+    fieldnames = ["id", "title", "body"]
 
-    with open("file.csv", mode="w", newline="") as file_csv
-
-    writer = csv.DictWriter(file_csv, filedname)
+    with open("file.csv", mode="w", newline="") as file_csv:
+        writer = csv.DictWriter(file_csv, fieldnames=fieldnames)
 
     writer.writeheader()
 
-    writer.writerow(structured_data)
+    for data in structured_data:
+        writer.writerow(data)
