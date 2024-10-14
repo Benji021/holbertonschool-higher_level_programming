6d9f966e26290f96bacd8f970bbbf99c25342f82
Benji021 <9548holbertonstudents.com>
2024-10-14 10:54:46 +0200

    request

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -35,8 +35,8 @@ def fetch_and_save_posts():
             } for post in posts]
 
 
-    with open("posts.csv", mode="w", newline="", encoding="utf-8") as file:
-        writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+    with open("posts.csv", mode="w", newline="", encoding="utf-8") as dic:
+        writer = csv.DictWriter(dic, fieldnames=["id", "title", "body"])
 
         writer.writeheader()
 
