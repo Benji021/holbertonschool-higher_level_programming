434c5960c363998b05466f74f858afc0634c9ff2
Benji021 <9548holbertonstudents.com>
2024-10-17 11:27:51 +0200

    server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -50,9 +50,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
-            err_ = {
-                "404 Not Found"
-            }
+            err_ = "Not Found"
             self.wfile.write(json.dump(err_))
 
             #self.wfile.write(b'404 Not Found')
