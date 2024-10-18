003afd3b424eccf3d97bb951e788ca3fc6325af8
Benji021 <9548holbertonstudents.com>
2024-10-18 09:37:18 +0200

    Flask

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -38,22 +38,23 @@ def get_user(username):
 @app.route('/add_user', methods=['POST'])
 def add_user():
     data = request.get_json()
-    username = data.get('username')
-    name = data.get("name")
-    age = data.get("age")
-    city = data.get("city")
 
-    if username and username not in users and name and isinstance(age, int) and city:
-            users[username] = {
-                "name": name,
-                "age": age,
-                "city": city
-            }
-            return jsonify(
-                {"message": "User added", "user": users[username]}), 201
-    else:
-        return jsonify(
-            {"error": "Invalid data or user already exists"}), 400
+    if "username" not in data:
+        return jsonify({"error": "Username is required"}), 400
+    
+    username = data["username"]
+
+    users["username"] = {
+        "username" = username,
+        name = data.get("name"),
+        age = data.get("age"),
+        city = data.get("city")
+    }
+
+    return jsonify({
+        "message": "User added",
+        "user": users[username]
+    }), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
