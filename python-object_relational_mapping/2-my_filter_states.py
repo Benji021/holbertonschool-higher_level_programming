bdc039eaca6879bc4314fe50f42d544977a8336b
Benji021 <9548holbertonstudents.com>
2024-10-29 15:11:28 +0100

    Argument and Displays all value

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -2,20 +2,36 @@
 """ List all states with a name starting with N from the hbtn_0e_0_usa """
 
 import MySQLdb
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
-    query_rows = cur.fetchall()
+# Get arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    for row in query_rows:
-        if ('N' in row[1]):
-            print(row)
-    cur.close()
+    # Connect to the MySQL database
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                         passwd=mysql_password, db=database_name)
+
+    cursor = db.cursor()
+
+    # Create the SQL query using format
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Display the results
+    for row in results:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
     db.close()
