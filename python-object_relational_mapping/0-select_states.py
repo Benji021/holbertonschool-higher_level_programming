bd6e261063d420f84df5193fcd4889f471fae5f6
Benji021 <9548holbertonstudents.com>
2024-10-29 10:58:19 +0100

    List all states from database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,13 +5,34 @@ script that lists all states from the database hbtn_0e_0_usa
 import sys
 import MySQLdb # type: ignore
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+def main():
+    # Checking arguments
+    if len(sys.argv) != 4:
+        print("Usage: ./script.py <mysql_username> <mysql_password> <database_name>")
+        return
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states;")
-    states = cur.fetchall()
+    # Retrieving arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-    for state in states:
+    # Database connexion
+    db = MySQLdb.connect(host="localhost", user=mysql_username, passwd=mysql_password, db=database_name, port=3306)
+
+    # Cursor creation
+    cursor = db.cursor()
+
+    # Execute SQL query to retrieve reports sorted by id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Retrieving and displaying results
+    results = cursor.fetchall()
+    for state in results:
         print(state)
+
+    # Close to connexion
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
