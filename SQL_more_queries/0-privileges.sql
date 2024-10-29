a99503ab64d7832c9509d24a79eaf51198fee97c
Benji021 <9548holbertonstudents.com>
2024-10-29 10:16:20 +0100

    List all privilege

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- List all privileges of the MYSQL users
+SELECT User, Host FROM mysql.user WHERE User IN ('user_0d_1', 'user_0d_2');
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
