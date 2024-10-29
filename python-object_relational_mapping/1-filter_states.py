83865003909f20a197c82059a27295ae11773bfc
Benji021 <9548holbertonstudents.com>
2024-10-29 14:16:26 +0100

    List all states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
         passwd=password, db=database)
-    
+
     cursor = db.cursor()
 
     query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
@@ -22,6 +22,6 @@ if __name__ == "__main__":
     states = cursor.fetchall()
     for state in states:
         print(state)
-              
+
     cursor.close()
     db.close()
