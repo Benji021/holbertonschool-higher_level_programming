dd3e13f5dcc7703731c65680c806375690e09123
Benji021 <9548holbertonstudents.com>
2024-10-29 11:05:38 +0100

    List all states from database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,23 +16,28 @@ def main():
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    # Database connexion
-    db = MySQLdb.connect(host="localhost", user=mysql_username, passwd=mysql_password, db=database_name, port=3306)
+    try:
+        # Database connexion
+        db = MySQLdb.connect(host="localhost", user=mysql_username, passwd=mysql_password, db=database_name, port=3306)
 
-    # Cursor creation
-    cursor = db.cursor()
+        # Cursor creation
+        cursor = db.cursor()
 
-    # Execute SQL query to retrieve reports sorted by id
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        # Execute SQL query to retrieve reports sorted by id
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Retrieving and displaying results
-    results = cursor.fetchall()
-    for state in results:
-        print(state)
+        # Retrieving and displaying results
+        results = cursor.fetchall()
+        for state in results:
+            print(state)
 
-    # Close to connexion
-    cursor.close()
-    db.close()
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        # Close to connexion
+        cursor.close()
+        db.close()
 
 if __name__ == "__main__":
     main()
