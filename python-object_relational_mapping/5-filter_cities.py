41bace8013c25aba42eaa590ebb885776cb24b78
Benji021 <9548holbertonstudents.com>
2024-10-30 09:38:58 +0100

    All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -5,12 +5,23 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
+
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <database> <state_name>")
+        sys.exit(1)
+
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
+
     cur = db.cursor()
+
     cur.execute("SELECT cities.id, cities.name, states.name \
         FROM cities JOIN states ON cities.state_id = states.id \
             WHERE states.name = '{}';".format(sys.argv[4]))
+
     states = cur.fetchall()
 
     print(", ".join([state[1] for state in states]))
+
+    cur.close()
+    db.close()
