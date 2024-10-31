02d54c402547998d2c494dc4ce91c4a7369c945a
Benji021 <9548holbertonstudents.com>
2024-10-31 09:27:21 +0100

    Filter States

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -12,6 +12,9 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
+    db = None
+    cursor = None
+
     try:
         db = MySQLdb.connect(host="localhost", port=3306, user=username,
             passwd=password, db=database)
