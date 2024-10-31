5aa1efaf8bac41fb98642f37a2d480625a95c7e2
Benji021 <9548holbertonstudents.com>
2024-10-31 16:52:51 +0100

    Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,16 +1,42 @@
 #!/usr/bin/python3
-"""SQL Injection..."""
+"List all cities from database hbtn_0e_4_usa"
 
 import sys
 import MySQLdb
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name \
-        FROM cities JOIN states ON cities.state_id = states.id;")
-    states = cur.fetchall()
+def main():
+    # Checking arguments
+    if len(sys.argv) != 4:
+        print("Usage: ./script.py <mysql_username> <mysql_password> <database_name>")
+        return
 
-    for state in states:
-        print(state)
+    # Retrieving arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    try:
+        # Database connexion
+        db = MySQLdb.connect(host="localhost", user=mysql_username, passwd=mysql_password, db=database_name, port=3306)
+
+        # Cursor creation
+        cursor = db.cursor()
+
+        # Execute SQL query to retrieve reports sorted by id
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+        # Retrieving and displaying results
+        results = cursor.fetchall()
+        for state in results:
+            print(state)
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        # Close to connexion
+        cursor.close()
+        db.close()
+
+if __name__ == "__main__":
+    main()
