e428d4f7b3db831febd067b15a27837db24e8098
Benji021 <9548holbertonstudents.com>
2024-10-31 15:52:24 +0100

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,33 +5,38 @@ import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
+def list_states():
 
-    # Get arguments
+    # Retrieving arguments
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to the MySQL database
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                         passwd=mysql_password, db=database_name)
+    try:
+        # Connect to the MySQL database
+        db = MySQLdb.connect(host="localhost", user=mysql_username,
+                        passwd=mysql_password, db=database_name, port=3306)
 
-    cursor = db.cursor()
+        cursor = db.cursor()
 
-    # Create the SQL query using format
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+        # Execute the query
+        cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name))
 
-    # Execute the query
-    cursor.execute(query)
+        # Fetch all results
+        results = cursor.fetchall()
 
-    # Fetch all results
-    results = cursor.fetchall()
+        # Display the results
+        for row in results:
+            print(row)
 
-    # Display the results
-    for row in results:
-        print(row)
+    except MySQLdb.Error as e:
+        print(f"SQL connection or execution error : {e}")
 
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+    finally:
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
+
+if __name__ == "__main__":
+    list_states()
