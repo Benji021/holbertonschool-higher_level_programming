5f3744979ebfb693143f9d28406c70c96fdf6ff0
Benji021 <9548holbertonstudents.com>
2024-10-31 15:16:20 +0100

    filter States

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,21 +6,30 @@ import sys
 import MySQLdb
 
 
-
-if __name__ == "__main__":
-
+def main():
+    # Checking arguments
+    if len(sys.argv) != 4:
+        print("Usage: ./script.py <mysql_username> <mysql_password> <database_name>")
+        return
+    
+    # Retrieving arguments
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
 
     try:
+        # Database connexion
         db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
             passwd=mysql_password, db=database_name)
 
+        #cursor creation
         cursor = db.cursor()
 
+        #Execute SQL query to retrieve reports sorted by id
         cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+
+        # Retrieving and displaying results
         states = cursor.fetchall()
         for state in states:
             print(state)
@@ -29,7 +38,11 @@ if __name__ == "__main__":
         print(f"Error {e}")
 
     finally:
+        # Close to connexion
         if cursor:
             cursor.close()
         if db:
             db.close()
+
+if __name__ == "__main__":
+    main()
