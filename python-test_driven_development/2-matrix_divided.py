4016ce6b167e1f46f278432f610724edd3873f9f
Benji021 <benjaminpignolet@orange.fr>
2025-01-23 16:20:50 +0100

    Divides all elements of a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -26,8 +26,7 @@ def matrix_divided(matrix, div):
         not all(isinstance(row, list) for row in matrix) or
         not all(isinstance(element, (int, float))
                 for row in matrix for element in row)):
-        raise TypeError("matrix must be a matrix (list of lists)"
-                        "of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
