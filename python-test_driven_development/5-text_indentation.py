057aa22ae9e451d2a2e656a4a00c0cadab0029ed
Benji021 <benjaminpignolet@orange.fr>
2025-01-24 10:24:45 +0100

    Prints a text with 2 new lines after each of these characters: ., ? and :

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,22 @@
-m
+#!/usr/bin/python3
+"""
+This module does X, Y, and Z.
+"""
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    result = ""
+    skip_space = True
+
+    for char in text:
+        result += char
+        if char in ".?:":
+            result += "\n\n"
+            skip_space = True
+        elif char == " " and skip_space:
+            continue
+        else:
+            skip_space = False
+
+        print(result.strip())
