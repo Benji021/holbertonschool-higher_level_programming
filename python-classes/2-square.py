a9fb88106cab17694aacb78bf3bdc21463180286
Benji021 <benjaminpignolet@orange.fr>
2025-01-28 11:38:15 +0100

    df

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,1 @@
-#!/usr/bin/python3
-"""Define a square"""
-
-
-class Square:
-    """Empty class that define a square"""
-
-    def __init__(self, size=0):
-        """Initialize a new square with an optional size"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+df
