caedc458aa9207b3c4fcf183e62b9575a5d4b2e8
Benji021 <benjaminpignolet@orange.fr>
2025-01-28 11:37:42 +0100

    df

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,15 +1,1 @@
-#!/usr/bin/python3
-"""Defines a square"""
-
-
-class Square:
-    """defines a square"""
-
-    def __init__(self, size):
-        """
-        Initializes the square with a private size attribute
-
-        Args:
-        size (int): size of the square
-        """
-        self.__size = size
+df
