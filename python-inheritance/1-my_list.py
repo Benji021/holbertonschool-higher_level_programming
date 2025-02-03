0d59de359a5d12a4c0c7a2bd77badda51e31b999
Benji021 <benjaminpignolet@orange.fr>
2025-02-03 10:53:13 +0100

    My list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defining a class MyList"""
+""" This is docstring MyList"""
 
 
 class MyList(list):
-    """Class MyList that inherits from list"""
+    """ Define MyList that inheritance list """
 
-    def print_sorted(self):
-        """Print sorted list"""
-        print(sorted(self))
+def print_sorted(self):
+    """ Print the list, but sorted ascensing sort """
+    print(sorted(self))
