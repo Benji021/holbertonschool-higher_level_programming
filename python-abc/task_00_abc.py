b2349d46f916da44f542f821c4f3ac5206caf721
Benji021 <benjaminpignolet@orange.fr>
2025-02-04 10:14:48 +0100

    Abstract Animal Class and its Subclasses

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,1 +1,20 @@
-gf
+#!/usr/bin/python3
+"""Docstring Abstract animal class and its subclasses"""
+
+
+from abc import ABC
+
+
+class Animal(ABC):
+
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+    
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
