33406034be1f9b75ea0794971bb50cca361dd699
Benji021 <9548holbertonstudents.com>
2025-02-05 15:01:01 +0100

    Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ def inherits_from(obj, a_class):
     """ Ckeck if obj is an instance of a_class that inherited
     directly or indirectly from the specified class 
     """
-    return isinstance(obj, a_class) and type (obj) is not a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
