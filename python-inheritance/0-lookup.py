e02b9a5a7c2a9cb36f60f00c700cb10bcb7da171
Benji021 <benjaminpignolet@orange.fr>
2025-02-05 10:32:32 +0100

    Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -7,6 +7,6 @@ def lookup(obj):
     Return a liste of available attributes and methods of an object
     
     param obj : the object to inspect
-    return : List of attributes and methods an object
+    return : A list of attribute and method names
     """
     return dir(obj)
