abb151a8888348e041a3529cc222e0621a1023e6
Benji021 <benjaminpignolet@orange.fr>
2025-02-05 10:24:07 +0100

    Shapes, Interfaces, and Duck Typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -48,5 +48,5 @@ if __name__ == "__main__":
     circle = Circle(5)
     rectangle = Rectangle(4, 6)
 
-    shape_info(Circle)
-    shape_info(Rectangle)
+    shape_info(circle)
+    shape_info(rectangle)
