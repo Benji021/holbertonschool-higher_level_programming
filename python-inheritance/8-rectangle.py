5615e03bba32ec03f2565e13c8ac50a69a648d33
Benji021 <9548holbertonstudents.com>
2025-02-05 15:37:42 +0100

    Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,35 +4,18 @@
 
 class BaseGeometry:
     """Empty class that defines BaseGeometry"""
-    pass
-
-class Rectangle(BaseGeometry):
-    """Define a rectangle"""
-
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self._width = width
-        self._height = height
-
-    def area(self):
-        """
-        Return the current area that raises an exeption
-        with message area() is not implemented
-        """
-        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
-        Set that validates value
+        Validate value
 
         Args: 
-            name (str): the name of parameter
-            value (int): validates value
+            name (str): the name of the parameter
+            value (int): the value to validate
 
         Raises:
-            TypeError: value is not integer;
-            ValueError: value is less or equal to 0;
+            TypeError: if value is not an integer
+            ValueError: if value is less than or equal to 0
         """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
@@ -40,11 +23,35 @@ class Rectangle(BaseGeometry):
             raise ValueError(f"{name} must be greater than 0")
 
 
+class Rectangle(BaseGeometry):
+    """Define a rectangle"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Returns the area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return a formatted string representation of the rectangle"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
 if __name__ == "__main__":
     try:
         r = Rectangle(3, 5)
-        r2 = Rectangle(4, True)
-        print(f"Rectangle created: width = {r._width}, height = {r._height}")
+        print(f"Rectangle created: width = {r._Rectangle__width}, height = {r._Rectangle__height}")
         print(issubclass(Rectangle, BaseGeometry))
+
+        print(f"Area: {r.area()}")
+
+        print(r)
+
     except Exception as e:
         print(f"Error: {e}")
