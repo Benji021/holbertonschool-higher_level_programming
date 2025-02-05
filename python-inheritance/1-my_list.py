d5605c3d02058035cc4f946b2aa90f0d9ef4bcb9
Benji021 <benjaminpignolet@orange.fr>
2025-02-05 10:34:22 +0100

    My list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,6 @@
 class MyList(list):
     """ Define MyList that inheritance list """
 
-def print_sorted(self):
-    """ Print the list, but sorted ascensing sort """
-    print(sorted(self))
+    def print_sorted(self):
+        """ Print the list, but sorted ascensing sort """
+        print(sorted(self))
