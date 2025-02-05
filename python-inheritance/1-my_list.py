678f2f87f33fa415467dad8eb4842ce44ba6f8dd
Benji021 <benjaminpignolet@orange.fr>
2025-02-05 10:39:22 +0100

    My list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,7 +8,3 @@ class MyList(list):
     def print_sorted(self):
         """ Print the list, but sorted ascensing sort """
         print(sorted(self))
-
-if __name__ == "__main__":
-    my_list = MyList([3, 1, 4, 1, 5, 9, 2, 6])
-    my_list.print_sorted()  # Affichera la liste triée
