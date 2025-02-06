3a070a4973d81092b0ae77796719a8430448b658
Benji021 <9548holbertonstudents.com>
2025-02-06 10:15:50 +0100

    s

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -14,3 +14,22 @@ def inherits_from(obj, a_class):
         bool: True if obj is an instance of a subclass of a_class, False otherwise.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
+
+# Tests
+class A:
+    pass
+
+class B(A):
+    pass
+
+class C(B):
+    pass
+
+a = A()
+b = B()
+c = C()
+
+print(inherits_from(b, A))  # Doit afficher True
+print(inherits_from(c, A))  # Doit afficher True
+print(inherits_from(a, A))  # Doit afficher False
+print(inherits_from(b, B))  # Doit afficher False
