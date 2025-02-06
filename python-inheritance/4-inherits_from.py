8258f05afd1074f785bd4f76cf97d79f6f6e326e
Benji021 <9548holbertonstudents.com>
2025-02-06 10:18:04 +0100

    Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -14,3 +14,5 @@ def inherits_from(obj, a_class):
         bool: True if obj is an instance of a subclass of a_class, False otherwise.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
+
+if __name__ == "__main__":
