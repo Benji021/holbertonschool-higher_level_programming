d29926a42f01ec974bd88fb98ad3c9630cc802d7
Benji021 <9548holbertonstudents.com>
2025-02-10 10:37:26 +0100

    g

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,1 @@
-#!/usr/bin/python3
-"""Docstring Write to a file to UTF8"""
-
-
-def write_file(filename="", text=""):
-
-    with open(filename, "w", encoding="utf-8") as file:
-        return file.write(text)
+g
