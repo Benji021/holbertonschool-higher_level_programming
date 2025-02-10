ea7f956ad1a658cfa1c84474e80d5d75f32daeb3
Benji021 <9548holbertonstudents.com>
2025-02-10 11:08:10 +0100

    Save object to a file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,9 @@
-g
+#!/usr/bin/python3
+"""Docstring Save object to a file"""
+
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
