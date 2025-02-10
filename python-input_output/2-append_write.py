2e38f625f2f844a0d5ece3632495359799bde1f6
Benji021 <9548holbertonstudents.com>
2025-02-10 10:43:43 +0100

    Append to a file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,9 @@
-g
+#!/usr/bin/python3
+"Docstring Append to a file to UTF8"
+
+
+def append_write(filename="", text=""):
+
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write(text)
+    return len(text)
