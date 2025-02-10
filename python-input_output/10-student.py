6c949fbf025ef1cae7389be98994a0c0807d1cc6
Benji021 <9548holbertonstudents.com>
2025-02-10 14:07:18 +0100

    Student to JSON with filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,15 @@
-g
+#!/usr/bin/python3
+"""Docstring Student to JSON with filter"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        return {attr: self.__dict__[attr] for attr in attrs if attr in self.__dict__}
