e5c83fb487f9e3dbfd5c622fae44438ce67a1bd5
Benji021 <9548holbertonstudents.com>
2025-02-10 11:31:11 +0100

    Save object to a file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,19 @@
-g
+#!/usr/bin/python3
+"""Docstring Load, add and save to JSON file"""
+
+
+import sys
+from os.path import exists
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+if exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+
+save_to_json_file(my_list, filename)
