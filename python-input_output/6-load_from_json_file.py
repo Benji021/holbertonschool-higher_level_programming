e78550ed41bdc96d1829d0da075c9f527f6833d6
Benji021 <9548holbertonstudents.com>
2025-02-10 11:15:07 +0100

    g

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,8 +1,1 @@
-#!/usr/bin/python3
-"""Docstring create object from a JSON file"""
-
-import json
-
-def load_from_json_file(filename):
-    with open(filename, "r") as file:
-        return json.load(file)
+g
