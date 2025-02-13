e7a64486bcba99a63d46c6ab900e96e5916ddf5d
Benji021 <9548holbertonstudents.com>
2025-02-13 16:13:47 +0100

    Pascal's triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,14 +3,7 @@
 
 
 def pascal_triangle(n):
-    """Generate Pascal's triangle up to the nth row.
-    
-    Args:
-        n (int): Number of rows in the triangle.
-    
-    Returns:
-        List[List[int]]: Pascal's triangle as a list of lists.
-    """
+    """Returns a list of lists representing Pascal's Triangle of size n."""
     if n <= 0:
         return []
     
@@ -18,7 +11,12 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         prev_row = triangle[-1]
-        new_row = [1] + [prev_row[j - 1] + prev_row[j] for j in range(1, len(prev_row))] + [1]
+        new_row = [1]
+
+        for j in range(1, len(prev_row)):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)
         triangle.append(new_row)
 
     return triangle
