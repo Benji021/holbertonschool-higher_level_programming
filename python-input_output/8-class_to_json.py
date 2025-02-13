e293e9b5929133d5de88e5c2cd4beea6dbf8b5ca
Benji021 <9548holbertonstudents.com>
2025-02-13 14:57:21 +0100

    Class to JSON

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,9 +2,13 @@
 """Module that provides a function to serialize a class instance into JSON-compatible format"""
 
 
+import json
 def class_to_json(obj):
     """
     Returns the dictionary description of an object for JSON serialization.
     The function assumes all attributes are serializable (list, dict, str, int, bool).
     """
-    return obj.__dict__
+    try:
+        return json.loads(json.dumps(obj.__dict__))
+    except AttributeError:
+        raise TypeError(f"Object of type {type(obj).__name__} is not serializable")
