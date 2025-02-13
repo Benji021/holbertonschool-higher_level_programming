bc622d4725045f5d0018cbf324b98c9f27e5dd56
Benji021 <9548holbertonstudents.com>
2025-02-13 09:40:59 +0100

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,10 +4,5 @@
 
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout"""
-    try:
-        with open(filename, "r", encoding="utf_8") as f:
-            print(f.read(), end="")
-    except FileNotFoundError:
-        print(f"error: file '{filename}' doesn't exist")
-    except UnicodeDecodeError:
-        print(f"error: do not read '{filename}', encoding problem")
+    with open(filename, "r", encoding="utf_8") as f:
+        print(f.read(), end="")
