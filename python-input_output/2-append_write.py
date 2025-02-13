c4de00c163ee0877e850506f4e0662fdce7c5f49
Benji021 <9548holbertonstudents.com>
2025-02-13 09:45:59 +0100

    Append to a file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 
 
 def append_write(filename="", text=""):
-
+    """Append a UTF-8 text file and prints its content to stdout"""
     with open(filename, "a", encoding="utf-8") as file:
         file.write(text)
     return len(text)
