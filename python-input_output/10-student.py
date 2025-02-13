a588c401bf107717cee2c5361be0e0a7039884ce
Benji021 <9548holbertonstudents.com>
2025-02-13 15:21:49 +0100

    Student to JSON with filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -3,16 +3,13 @@
 
 
 class Student:
-
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-       """Return a dictionary representation of the Student"""
-       if attrs is None:
+        if attrs is None:
             return self.__dict__
 
         if not isinstance(attrs, list) or not all(isinstance(attr, str) for attr in attrs):
