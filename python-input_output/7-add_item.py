271f6d0c91e4cdd8cd0384fdff86ece7e9813d69
Benji021 <9548holbertonstudents.com>
2025-02-13 13:48:17 +0100

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,19 +2,15 @@
 """Docstring Load, add and save to JSON file"""
 
 
-import sys
-import os
+from sys import argv
 
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+try:
+    arglist = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    arglist = []
 
-if os.path.exists(filename):
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
-
-my_list.extend(sys.argv[1:])
-
-save_to_json_file(my_list, filename)
+arglist += argv[1:]
+save_to_json_file(arglist, "add_item.json")
