bcdc4b0f6b22c9af62a230e1f5a515489a23d843
Benji021 <9548holbertonstudents.com>
2025-02-13 15:33:25 +0100

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout"""
-    with open(filename, "r", encoding="utf_8", errors="replace") as f:
+    with open(filename, "r", encoding="utf_8") as f:
         print(f.read(), end="")
