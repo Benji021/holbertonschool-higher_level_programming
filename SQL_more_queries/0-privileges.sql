acbc0cafdc2b33e2b1208c7442cedf84d2b9380f
Benji021 <9548holbertonstudents.com>
2025-03-05 10:19:44 +0100

    My privilege!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,24 +1,3 @@
-import MySQLdb
-
-# MySQL database connection
-conn = MySQLdb.connect(
-    host="localhost",
-    user="root",  # Replace with your admin user if necessary
-    passwd="password",  # Enter your password
-    database="mysql"  # The database where privileges are stored
-)
-
-cursor = conn.cursor()
-
-# Exécuter les commandes
-users = ["user_0d_1", "user_0d_2"]
-for user in users:
-    cursor.execute(f"SHOW GRANTS FOR '{user}'@'localhost';")
-    grants = cursor.fetchall()
-    
-    print(f"Privilèges pour {user}:")
-    for grant in grants:
-        print(grant[0])
-
-cursor.close()
-conn.close()
+-- List all privileges of the MYSQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
