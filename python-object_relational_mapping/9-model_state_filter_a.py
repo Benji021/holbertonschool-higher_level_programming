2234f04081739002b5bb609d8472db9662d13654
Benji021 <9548holbertonstudents.com>
2025-03-06 16:29:28 +0100

    Contains 'a'

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,13 +9,20 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+        Session = sessionmaker(bind=engine)
+        session = Session()
 
-    states = session.query(State).filter(State.name.ilike('%a%')).order_by(State.id).all()
+        states = session.query(State).filter(State.name.ilike('%a%')).order_by(State.id).all()
 
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+
+    except Exception as e:
+        print("Error:", e)
+
+    finally:
+        session.close()
