814ad18bd9391afb1d60f230db0c62e7e0cbc9bb
Benji021 <9548holbertonstudents.com>
2025-03-06 12:08:08 +0100

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,37 +1,18 @@
 #!/usr/bin/python3
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-import sys
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 
-def list_states(mysql_user, mysql_password, db_name):
-    # Create the engine to connect to the MySQL server
-    engine = create_engine(f'mysql+mysqldb://{mysql_user}:{mysql_password}@localhost:3306/{db_name}')
-    
-    # Create all tables if they don't already exist (using Base metadata)
-    Base.metadata.create_all(engine)
-    
-    # Create a session to interact with the database
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    
-    # Query all State objects and order by id in ascending order
-    states = session.query(State).order_by(State.id).all()
-    
-    # Loop through the results and print each State object
-    for state in states:
-        print(f"{state.id}: {state.name}")
-    
-    # Close the session
-    session.close()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
-if __name__ == '__main__':
-    # Check if the script is being executed directly
-    if len(sys.argv) == 4:
-        mysql_user = sys.argv[1]
-        mysql_password = sys.argv[2]
-        db_name = sys.argv[3]
-        
-        list_states(mysql_user, mysql_password, db_name)
-    else:
-        print("Usage: ./list_states.py <mysql_username> <mysql_password> <database_name>")
+
+class State(Base):
+    """
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
