27755e04b07f6075e7692c01e943b4cfd580fb17
Benji021 <9548holbertonstudents.com>
2025-03-06 10:25:44 +0100

    All states via SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
 """
-Lists all State objects from the database hbtn_0e_6_usa,
-sorted by ascending id.
+Lists all State objects from the database hbtn_0e_6_usa
 """
+import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-import sys
 
 
 if __name__ == '__main__':
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Establish a connection to the MySQL database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        mysql_username, mysql_password, database_name), pool_pre_ping=True)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    """Query all State objects from the database,
-    ordered by id in ascending order"""
     states = session.query(State).order_by(State.id).all()
 
     for state in states:
-        print(f"{state.id}: {state.name}")
-
-    session.close()
+        print("{}: {}".format(state.id, state.name))
