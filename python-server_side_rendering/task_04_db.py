d8a68f94e7b047c3c28784b49bdac0a5287c1b08
Benji021 <9548holbertonstudents.com>
2025-04-04 11:56:41 +0200

    Extending Dynamic Data Display to Include SQLite in Flask

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -3,4 +3,4 @@
     {"id": 2, "name": "Coffee Mug", "category": "Home Goods", "price": 15.99},
     {"id": 3, "name": "Headphones", "category": "Electronics", "price": 129.99},
     {"id": 4, "name": "Notebook", "category": "Office Supplies", "price": 4.99}
-] 
+]
